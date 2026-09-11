5fee76755f5eb7fdf3f438f57e5617b4c28e0f95
mclainv <rma9540@nyu.edu>
2026-09-11 09:12:03 -0400

    Added default react project layout

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Graph from './components/graph';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Public Stock API</h1>
+            <Graph />
+        </div>
+    );
+};
+
+export default App;
---
+++ client/src/components/graph.jsx
@@ -0,0 +1,7 @@
+export default function Graph() {
+    return (
+        <div>
+            <h1>Graph</h1>
+        </div>
+    );
+}
