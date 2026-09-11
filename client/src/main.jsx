7aa0050678452ac0f8a88a405bd39904032a7dca
McLain Adams <thedystopyansociety@gmail.com>
2026-09-11 10:49:17 -0400

    Updated app.jsx to import index.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // optional: global styles
+import './styles/index.css'; // optional: global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
