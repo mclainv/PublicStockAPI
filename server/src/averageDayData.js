1ea246dce56bf4545a49f17695a2968c555e3a76
mclainv <rma9540@nyu.edu>
2026-09-10 20:22:18 -0400

    Added an averageDayData function, which takes a map of Date and Data and returns an array of objects of the format {date, lowAverage, highAverage}

---
+++ server/src/averageDayData.js
@@ -0,0 +1,19 @@
+export default function averageDayData(dataByDay) {
+    const averageData = [];
+
+    dataByDay.forEach((day, date) => {
+        const {low = 0, high = 0, missingLow = 0, missingHigh = 0} = day.reduce(({low, high}, currentDay) => {
+            low += (currentDay.low == null ? 0 : currentDay.low);
+            if (currentDay.low == null) missingLow++;
+            high += (currentDay.high == null ? 0 : currentDay.high); 
+            if (currentDay.high == null) missingHigh++;
+            return {low, high};
+        }, {low: 0, high: 0});
+        averageData.push({
+            date,
+            low: low / (day.length - missingLow),
+            high: high / (day.length - missingHigh)
+        })
+    });
+    return averageData;
+}
